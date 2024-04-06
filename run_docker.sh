db95239be5b7851c9114926515e6c7fc8fb8fe0d
Geon <114966864+geon0430@users.noreply.github.com>
2024-04-07 00:06:12 +0900

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -6,24 +6,20 @@ IMAGE_NAME="paddle_RT-DETR_model_build_onnx"
 TAG="0.1"
 
 port_num="1"
-fastapiUiUx_path=$(pwd)
+RT-DETR-paddle-model-build-onnx_path=$(pwd)
 
 
 docker run \
     --runtime nvidia \
     --gpus all \
     -it \
-    -p ${port_num}1575:1554 \
-    -p ${port_num}6000:8000 \
-    -p ${port_num}7000:9000 \
-    -p ${port_num}7777:8888 \
-    -p ${port_num}7444:8444 \
+    -p ${port_num}8888:8888 \
     --name ${CONTAINER_NAME} \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v ${fastapiUiUx_path}:/RT-DETR \
+    -v ${RT-DETR-paddle-model-build-onnx_path}:/RT-DETR-paddle-model-build-onnx \
     -e DISPLAY=$DISPLAY \
     --shm-size 20g \
     --restart=always \
-    -w /RT-DETR \
+    -w /RT-DETR-paddle-model-build-onnx \
     ${IMAGE_NAME}:${TAG}
