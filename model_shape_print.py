59a574b2a2c604f1a7cbc0dc9a124324eebec4fd
Geon <114966864+geon0430@users.noreply.github.com>
2024-04-06 15:09:32 +0900

    Update model_shape_print.py

--- model_shape_print.py
+++ model_shape_print.py
@@ -5,7 +5,7 @@ def print_model_input_names_and_shapes(model_path):
     for input in session.get_inputs():
         print("Name:", input.name, "Shape:", input.shape)
 
-model_path = "/python_object_detection_server/padder/test/model/test.onnx"
+model_path = "./model/rtdetr_r50vd_6x_coco.onnx"
 
 
 print_model_input_names_and_shapes(model_path)
